a15c053f267577f0c655d9b2cb47b9f0c0cb0675
UnDer-7 <mateus7532@gmail.com>
2026-01-29 00:34:29 -0300

    fix pdf processing for graalvm

--- src/main/java/br/com/gorillaroxo/sanjy/client/web/service/extractor/ExtractTextFromFilePdfStrategy.java
+++ src/main/java/br/com/gorillaroxo/sanjy/client/web/service/extractor/ExtractTextFromFilePdfStrategy.java
@@ -2,13 +2,14 @@ package br.com.gorillaroxo.sanjy.client.web.service.extractor;
 
 import br.com.gorillaroxo.sanjy.client.web.exception.FailToExtractTextFromPdfFileException;
 import br.com.gorillaroxo.sanjy.client.web.util.LogField;
-import java.io.IOException;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+import java.io.InputStream;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.logstash.logback.argument.StructuredArguments;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,15 +23,27 @@ public class ExtractTextFromFilePdfStrategy implements ExtractTextFromFileStrate
     public String extract(final MultipartFile file) {
         log.info(
                 LogField.Placeholders.FOUR.getPlaceholder(),
-                StructuredArguments.kv(LogField.MSG.label(), "Extracting text String from PDF file"),
+                StructuredArguments.kv(LogField.MSG.label(), "Extracting text String from PDF file using iText"),
                 StructuredArguments.kv(LogField.DIET_PLAN_FILE_NAME.label(), file.getOriginalFilename()),
                 StructuredArguments.kv(LogField.DIET_PLAN_FILE_CONTENT_TYPE.label(), file.getContentType()),
                 StructuredArguments.kv(LogField.DIET_PLAN_FILE_SIZE_BYTES.label(), file.getSize()));
 
-        try (PDDocument document = PDDocument.load(file.getInputStream())) {
-            PDFTextStripper stripper = new PDFTextStripper();
+        try (InputStream inputStream = file.getInputStream();
+                PdfReader reader = new PdfReader(inputStream);
+                PdfDocument pdfDocument = new PdfDocument(reader)) {
 
-            final String text = stripper.getText(document);
+            StringBuilder textBuilder = new StringBuilder();
+            int numberOfPages = pdfDocument.getNumberOfPages();
+
+            for (int i = 1; i <= numberOfPages; i++) {
+                String pageText = PdfTextExtractor.getTextFromPage(pdfDocument.getPage(i));
+                textBuilder.append(pageText);
+                if (i < numberOfPages) {
+                    textBuilder.append("\n");
+                }
+            }
+
+            final String text = textBuilder.toString();
 
             log.info(
                     LogField.Placeholders.FOUR.getPlaceholder(),
@@ -40,7 +53,7 @@ public class ExtractTextFromFilePdfStrategy implements ExtractTextFromFileStrate
                     StructuredArguments.kv(LogField.DIET_PLAN_FILE_SIZE_BYTES.label(), file.getSize()));
 
             return text;
-        } catch (final IOException e) {
+        } catch (final Exception e) {
             log.warn(
                     LogField.Placeholders.FIVE.getPlaceholder(),
                     StructuredArguments.kv(LogField.MSG.label(), "Fail to extract text from PDF file"),
